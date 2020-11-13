ef5458e96fe007f5d5a9783e9fc0bf01c7d7dd2d
Seok93 <gkdltjr@example.com>
2020-11-13 14:54:50 +0900

    add middleware(morgan, helmet, cookie-parser, body-parser)

--- index.js
+++ index.js
@@ -1,23 +1,25 @@
+
 import express from "express";
-// const express = require('express');
+import morgan from "morgan";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+
 const app = express();
 
 const PORT = 4000;
 
 const handleListening = () => console.log(`Listening on: http://localhost:${PORT}`);
-// function handleListening() {
-//     console.log(`Listening on: http://localhost:${PORT}`);
-// }
 
 const handleHome = (req, res) => res.send("Hello From Home!");
-// function handleHome(req, res) {
-//     res.send("Hello From Home!");
-// }
 
 const handleProfile = (req, res) => res.send("You From Profile!");
-// function handleProfile(req, res) {
-//     res.send("You From Profile!");
-// }
+
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(helmet());
+app.use(morgan("dev"));
 
 app.listen(PORT, handleListening);
 app.get("/", handleHome);
