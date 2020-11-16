8e5dc55f8b20a1cea737a9af3c8a2b0ac11ad579
Seok93 <gkdltjr@example.com>
2020-11-16 13:37:14 +0900

    Use Router for Routing

--- app.js
+++ app.js
@@ -4,23 +4,23 @@ import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
+import { userRouter } from "./userRouter";
 
 const app = express();
 
-const PORT = 4000;
-
-const handleListening = () => console.log(`Listening on: http://localhost:${PORT}`);
-
 const handleHome = (req, res) => res.send("Hello From Home!");
-
 const handleProfile = (req, res) => res.send("You From Profile!");
 
+// Middleware 추가
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(helmet());
 app.use(morgan("dev"));
 
-app.listen(PORT, handleListening);
+// Router 설정
 app.get("/", handleHome);
 app.get("/profile", handleProfile);
+app.use("/user", userRouter); 
+
+export default app;
