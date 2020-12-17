d14ad09ec1c1d0203dcfca5eaa85a558bdba99a9
Seok93 <gkdltjr@example.com>
2020-12-17 16:50:32 +0900

    MVC Practice with Router

--- README.md
+++ README.md
@@ -4,8 +4,21 @@
 
 # 프로젝트 버전
 
-|SW|버전|
-|:---------------|:----------------|
-|NodeJS | v14.15.0 |
-|Express | v4.17.1 |
-|babel/node | v7.12.6 |
+| SW         | 버전     |
+| :--------- | :------- |
+| NodeJS     | v14.15.0 |
+| Express    | v4.17.1  |
+| babel/node | v7.12.6  |
+
+## Pages:
+
+- [ ] Home
+- [x] Join
+- [x] Login
+- [x] Search
+- [ ] User Detail
+- [ ] Edit Profile
+- [ ] Change Password
+- [ ] Upload
+- [ ] Video Detail
+- [ ] Edit Video
--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,12 +1,17 @@
 import express from "express";
 import routes from "../routes";
-import { users, editProfile, changePassword, userDetail } from "../controllers/userController";
+import {
+    users,
+    editProfile,
+    changePassword,
+    userDetail,
+} from "../controllers/userController";
 
 const userRouter = express.Router();
 
 userRouter.get(routes.users, users);
 userRouter.get(routes.editProfile, editProfile);
 userRouter.get(routes.changePassword, changePassword);
-userRouter.get(routes.userDetail, userDetail);
+userRouter.get(routes.userDetail(), userDetail);
 
 export default userRouter;
