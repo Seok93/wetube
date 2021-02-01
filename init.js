23dd8761d341d0c7d53176921e769dc1157ea121
Seok93 <gkdltjr@example.com>
2021-02-01 19:33:29 +0900

    local authentication with module

--- init.js
+++ init.js
@@ -1,12 +1,13 @@
-import app from "./app";
 import dotenv from "dotenv";
 import "./db";
+import app from "./app";
 import "./models/Video";
 import "./models/Comment";
+import "./models/User";
 
 dotenv.config();
 
-const PORT = process.env.PORT | 4000;
+const PORT = process.env.PORT || 4000;
 
 const handleListening = () =>
     console.log(`✅ Listening on: http://localhost:${PORT}`);
--- middlewares.js
+++ middlewares.js
@@ -4,12 +4,24 @@ import routes from "./routes";
 const multerVideo = multer({ dest: "uploads/videos/" });
 export const uploadVideoMiddleware = multerVideo.single("videoFile");
 
+export const onlyPublic = (req, res, next) => {
+    if (req.user) {
+        res.redirect(routes.home);
+    } else {
+        next();
+    }
+};
+
+export const onlyPrivate = (req, res, next) => {
+    if (req.user) {
+        next();
+    } else {
+        res.redirect(routes.home);
+    }
+};
 export const localsMiddleware = (req, res, next) => {
     res.locals.siteName = "WeTube";
     res.locals.routes = routes;
-    res.locals.user = {
-        isAuthenticated: false,
-        id: 1,
-    };
+    res.locals.user = req.user || null;
     next();
 };
