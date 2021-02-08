6b0a71d7a684dfd7865881b5faf0bb4005057fe9
Seok93 <gkdltjr@example.com>
2021-02-08 17:10:09 +0900

    Create Video Controls

---
+++ assets/js/videoPlayer.js
@@ -0,0 +1,49 @@
+const videoContainer = document.getElementById("jsVideoPlayer");
+const videoPlayer = document.querySelector("#jsVideoPlayer video");
+const playBtn = document.getElementById("jsPlayButton");
+const volumeBtn = document.getElementById("jsVolumeBtn");
+const fullScrnBtn = document.getElementById("jsFullScreen");
+
+function handlePlayClick() {
+    if (videoPlayer.paused) {
+        videoPlayer.play();
+        playBtn.innerHTML = '<i class="fas fa-pause"></i>';
+    } else {
+        videoPlayer.pause();
+        playBtn.innerHTML = '<i class="fas fa-play"></i>';
+    }
+}
+
+function handleVolumeClick() {
+    if (videoPlayer.muted) {
+        videoPlayer.muted = false;
+        volumeBtn.innerHTML = '<i class="fas fa-volume-up"></i>';
+    } else {
+        videoPlayer.muted = true;
+        volumeBtn.innerHTML = '<i class="fas fa-volume-mute"></i>';
+    }
+}
+
+function exitFullScreen() {
+    document.exitFullscreen();
+    fullScrnBtn.innerHTML = `<i class="fas fa-expand"></i>`;
+    fullScrnBtn.removeEventListener("click", exitFullScreen);
+    fullScrnBtn.addEventListener("click", goFullScreen);
+}
+
+function goFullScreen() {
+    videoContainer.requestFullscreen();
+    fullScrnBtn.innerHTML = `<i class="fas fa-compress"></i>`;
+    fullScrnBtn.removeEventListener("click", goFullScreen);
+    fullScrnBtn.addEventListener("click", exitFullScreen);
+}
+
+function init() {
+    playBtn.addEventListener("click", handlePlayClick);
+    volumeBtn.addEventListener("click", handleVolumeClick);
+    fullScrnBtn.addEventListener("click", goFullScreen);
+}
+
+if (videoContainer) {
+    init();
+}
